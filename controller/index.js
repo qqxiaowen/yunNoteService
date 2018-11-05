86e279776170b2511d3ef945ba1ff5e3975d1b4f
qqxiaowen <920049895@qq.com>
2018-11-05 20:12:37 +0800

    删除多余的文件

--- controller/index.js
+++ controller/index.js
@@ -1,17 +1,14 @@
 const {Router} = require("express")
 const router = Router()
-// const path = require('path')
 
 const login = require('./login')
 const user = require('./user')
 const article = require('./article')
 const category = require('./category')
-const token = require('./token')
 
 router.use(login);
 router.use(user);
 router.use(article);
 router.use(category);
-router.use(token);
 
 module.exports = router;
